19d0cf188129f3b5a720d68addcdda840ecc6aab
stanleyshi42 <stanleyshi42@gmail.com>
2024-06-18 13:20:24 -0700

    added tests

---
+++ src/test/java/com/example/jwt_restful_api/CustomerControllerTests.java
@@ -0,0 +1,40 @@
+package com.example.jwt_restful_api;
+
+import com.example.jwt_restful_api.controller.CustomerController;
+import com.example.jwt_restful_api.entity.Customer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class CustomerControllerTests {
+
+    @Autowired
+    CustomerController controller;
+
+    @Test
+    void contextLoads() {
+        assertThat(controller).isNotNull();
+    }
+
+    @Test
+    void testCRUD() {
+        String expectedUsername = "test";
+        String expectedPassword = "pass";
+        String expectedRoles = "ROLE_USER";
+        Customer expectedCustomer = new Customer(0, expectedUsername, expectedPassword, expectedRoles);
+        Customer actualCustomer = controller.addCustomer(expectedCustomer);
+
+        assertEquals(expectedUsername, actualCustomer.getUsername());
+        assertEquals(expectedPassword, actualCustomer.getPassword());
+        assertEquals(expectedRoles, actualCustomer.getRoles());
+
+        controller.deleteCustomerById(actualCustomer.getId());
+
+
+    }
+
+}
